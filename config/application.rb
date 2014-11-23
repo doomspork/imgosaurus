0640d4f40435e239d4e4e050dcf72bab2d19baa3
Sean Callan <seancallan@gmail.com>
2014-11-22 19:07:54 -0800

    Initial Rails project and dependencies

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require File.expand_path('../boot', __FILE__)
+
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Imgosaurus
+  class Application < Rails::Application
+
+  end
+end

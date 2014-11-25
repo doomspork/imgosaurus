81db236c4a9e9ec06c35bc184f91e8602fa90493
Sean Callan <seancallan@gmail.com>
2014-11-24 17:25:56 -0800

    Create ImagesController and create endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def bad_request
+    render nothing: true, status: 400
+  end
+
+  def created(image_path)
+    render json: { image: image_path }, status: 201
+  end
+
+  def internal_error
+    render nothing: true, status: 500
+  end
+
+  def missing?(*fields)
+    fields.any? { |field| params[field].nil? || params[field].empty? }
+  end
 end
---
+++ spec/controllers/images_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe ImagesController, type: :controller do
+  describe 'POST create' do
+    let(:final_image) { 'https://cdn.imgosaurus.com/image.png' }
+
+    it 'returns http success' do
+      mock(ImageProcessor).process(anything, anything) { final_image }
+      post :create, image: 'a remote path', transformations: { 'crop' => '15%' }
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)).to include 'image' => final_image
+    end
+
+    context 'when missing the image' do
+      it 'returns http bad request' do
+        post :create
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+  end
+
+end

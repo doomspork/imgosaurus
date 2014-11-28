96363c7388a213dba7fc193fd0bbac2812fa4bf1
Sean Callan <seancallan@gmail.com>
2014-11-28 12:42:40 -0800

    Initial factories for User and Image

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+# -*- encoding : utf-8 -*-
+FactoryGirl.define do
+  factory :user do
+    email 'user@example.com'
+    password 'passwords are great'
+    password_confirmation 'passwords are great'
+  end
+
+  factory :admin, class: User do
+    admin true
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
-
 require 'simplecov'
 require 'coveralls'
+require 'factory_girl'
 
 SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
 
@@ -14,20 +14,12 @@ RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-
+  config.include FactoryGirl::Syntax::Methods
   config.mock_with :rr
 
   config.disable_monkey_patching!
 
-  # Many RSpec users commonly either run the entire suite or an individual
-  # file, and it's useful to allow more verbose output when running an
-  # individual spec file.
-  if config.files_to_run.one?
-    # Use the documentation formatter for detailed output,
-    # unless a formatter has already been configured
-    # (e.g. via a command-line flag).
-    config.default_formatter = 'doc'
-  end
+  config.default_formatter = 'doc' if config.files_to_run.one?
 
   config.profile_examples = 10
 

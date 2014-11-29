b97638abbd8989b06eee6b9741f8227901d1773a
Sean Callan <seancallan@gmail.com>
2014-11-29 11:01:46 -0800

    Finish configuring Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # -*- encoding : utf-8 -*-
 Rails.application.routes.draw do
-  devise_for :users
   get 'home/index'
   root 'home#index'
 
   resources :images, only: %i(create)
+
+  # rubocop:disable Metrics/LineLength
+  devise_for :users, path: '/accounts', controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+  # rubocop:enable Metrics/LineLength
 end
